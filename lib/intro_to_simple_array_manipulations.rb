43093d6eb24ddc009e610eae8b55e355ff518589
Naga Bhargavi Marudhuri <bhargavimarudhuri@gmail.com>
2020-09-25 05:20:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,15 +1,4 @@
+
 def using_push(array, string)
-  array.push(string)
-end  
 
-def using_unshift(Array, String)
-   Array.unshift("abc")
-end  
-
-def using_pop(Array)
-   Array.pop
-end  
-
-def pop_with_args(Array, 2)
-   
-end  
+end
